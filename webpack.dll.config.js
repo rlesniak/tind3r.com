01bcf0197a4d46bbc919314f4d7a0708eaeb55f3
Rafal Lesniak <rafallesniak24@gmail.com>
2016-12-28 23:02:23 +0100

    bump version

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'source-map',
   entry: {
     vendor: [path.join(__dirname, 'src', 'vendors.js')]
   },

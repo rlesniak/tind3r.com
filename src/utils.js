2a436f3b790d9bcf981b7781db8dd5c5d1f586ad
Rafal Lesniak <rafallesniak24@gmail.com>
2017-01-17 20:28:25 +0100

    utils fix

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,12 @@
 export const miToKm = mi => Number((mi * 1.6093).toFixed(0))
 export const kmToMi = km => Number((km / 1.6093).toFixed(0))
+
+export const pageTitle = prefix => {
+  const title = 'Tind3r - Unofficial Tinder client'
+
+  if (prefix) {
+    return `${prefix} - ${title}`
+  }
+
+  return title
+}

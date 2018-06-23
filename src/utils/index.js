cbacaa8d4d92cb19094a8bd369f65186108170de
orman <orm@gefen.online>
2018-06-23 11:42:11 +0300

    eslint-disable-line

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,6 @@ export const miToKm = mi => Number((mi * 1.6093).toFixed(0));
 export const kmToMi = km => Number((km / 1.6093).toFixed(0));
 
 export const fbUserSearchUrl = (schoolId: string, name: string) =>
-  `https://www.facebook.com/search/str/${name}/keywords_users?filters_school={"name":"users_school","args":"${schoolId}"}`;
+  `https://www.facebook.com/search/str/${name}/keywords_users?filters_school={"name":"users_school","args":"${schoolId}"}`; // eslint-disable-line max-len
 
 export const pageTitle = 'Tind3r.com - Unofficial Tinder client';
